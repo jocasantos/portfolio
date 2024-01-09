d48ad7dda3419fff61388c68a449e56704e37af0
giga <tacuara10@gmail.com>
2024-01-09 16:18:50 +0000

    Add the Cards at about section.

--- portefolio-website/src/components/About.tsx
+++ portefolio-website/src/components/About.tsx
@@ -1,6 +1,7 @@
-import { Stack, Image } from "@chakra-ui/react";
+import { Stack, Image, Card, CardBody, Flex } from "@chakra-ui/react";
 import jocagray from "../assets/joca_blackandwhite.webp";
 import { Text } from "@chakra-ui/react";
+import { FaCode } from "react-icons/fa";
 
 const About = () => {
   return (
@@ -18,7 +19,42 @@ const About = () => {
         borderRadius={40}
         src={jocagray}
         alt="Joao Santos"
+        mb={2}
       />
+      <Card mb={1} w="360px" bg="white" color="black">
+        <CardBody>
+          <Flex justifyContent="center" mb={1}>
+            <FaCode />
+          </Flex>
+          <Flex flexDirection="column" align="center">
+            <Text mb={1} as="b">
+              Carrer Change
+            </Text>
+            <Text fontSize="sm" color="RGBA(0, 0, 0, 0.50)">
+              Decided to change carrers at 2023, from a Fitness Instructor with
+              a Masters Degree at Sports Science. Worked for 5+ years in a small
+              team enviroment, without a boss presence. As a hobby I'm a Futsal
+              Coach for kids for 10+ years.
+            </Text>
+          </Flex>
+        </CardBody>
+      </Card>
+      <Card w="360px" bg="white" color="black">
+        <CardBody>
+          <Flex justifyContent="center" mb={1}>
+            <FaCode />
+          </Flex>
+          <Flex flexDirection="column" align="center">
+            <Text mb={1} as="b">
+              Coding
+            </Text>
+            <Text fontSize="sm" color="RGBA(0, 0, 0, 0.50)">
+              I'm a self-learner, with focus on front-end development. After I
+              learn React, just started to...
+            </Text>
+          </Flex>
+        </CardBody>
+      </Card>
     </Stack>
   );
 };
