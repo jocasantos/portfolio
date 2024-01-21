e1b44e522561cd2efeb43ec904a253eaeb983802
giga <tacuara10@gmail.com>
2024-01-21 19:37:51 +0000

    Finish About Me page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -41,11 +41,11 @@ const About = () => {
             <Text mb={1} as="b">
               Carrer Change
             </Text>
-            <Text fontSize="sm" color="RGBA(0, 0, 0, 0.50)">
-              Decided to change carrers at 2023, from a Fitness Instructor with
-              a Master's degree at Sports Science. Worked for 5+ years in a
-              small team enviroment, without a boss presence. As a hobby I'm a
-              Futsal Coach for kids for 10+ years.
+            <Text textAlign="justify" fontSize="sm" color="RGBA(0, 0, 0, 0.50)">
+              Decided to change carrer at 2023, from a Fitness Instructor with a
+              Master's degree at Sports Science. Worked for 5+ years in a small
+              team enviroment, without a boss presence. Was a Kid's Futsal Coach
+              for 10+ years.
             </Text>
           </Flex>
         </CardBody>
@@ -59,9 +59,13 @@ const About = () => {
             <Text mb={1} as="b">
               Coding
             </Text>
-            <Text fontSize="sm" color="RGBA(0, 0, 0, 0.50)">
+            <Text textAlign="justify" fontSize="sm" color="RGBA(0, 0, 0, 0.50)">
               I'm a self-learner, with focus on front-end development. After I
-              learn React, just started to...
+              learned HTML, CSS and JavaScript, I started to learn{" "}
+              <b>React + TypeScript</b>... I did love it! Already tried
+              different UI libraries, like <b>Shadcn/UI</b>, <b>Chakra UI</b>,
+              <b> Bootstrap</b> and <b>Tailwind</b>. Right now I'm building a
+              full stack project with Next.JS.
             </Text>
           </Flex>
         </CardBody>
