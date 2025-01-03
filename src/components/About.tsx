819ffcf2d2a16824aae4f425394c3c09e167f66a
jocasantos <joaopcsantos1@gmail.com>
2025-01-03 16:12:27 +0000

    fix: refine About component description for improved clarity and flow

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -64,17 +64,12 @@ const About = () => {
               which laid the groundwork for building dynamic and responsive front-end applications. 
               I then advanced to <b>React</b> and <b>TypeScript</b>, honing my ability to create 
               robust, scalable user interfaces.<br></br>
-              <br></br> To deepen my technical skillset, I explored backend development with 
-               <b>Next.js</b>, focusing on essential concepts such as <b>Authentication</b>, 
+              <br></br> To deepen my technical skillset, I explored backend development with <b>Next.js</b>, focusing on essential concepts such as <b>Authentication</b>, 
               server-side rendering, and database management with both <b>SQL</b> and <b>NoSQL</b> solutions.<br></br>
-              <br></br> Currently, I am focused on expanding my knowledge in <b>DevOps</b> and 
-               <b>Cloud Computing</b>, integrating modern development and deployment practices into 
-              my skillset. My experience includes working with <b>AWS</b> services (<b>S3</b>, 
-               <b>EC2</b>, <b>VPC</b>, <b>IAM</b>, <b>DynamoDB</b>), containerization with <b>Docker</b>, infrastructure 
+              <br></br> Currently, I am focused on expanding my knowledge in <b>DevOps</b> and <b>Cloud Computing</b>, integrating modern development and deployment practices into 
+              my skillset. My experience includes working with <b>AWS</b> services (<b>S3</b>, <b>EC2</b>, <b>VPC</b>, <b>IAM</b>, <b>DynamoDB</b>), containerization with <b>Docker</b>, infrastructure 
               as code using <b>Terraform</b>, and configuration management with <b>Ansible</b>. 
-              I have also gained hands-on expertise in CI/CD tools like <b>Jenkins</b> and 
-               <b>GitHub Actions</b>, scripting with <b>Bash</b>, and leveraging <b>Git</b> and 
-               <b>GitHub</b> for version control and collaboration.<br></br>
+              I have also gained hands-on expertise in CI/CD tools like <b>Jenkins</b> and <b>GitHub Actions</b>, scripting with <b>Bash</b>, and leveraging <b>Git</b> and <b>GitHub</b> for version control and collaboration.<br></br>
               <br></br> With a strong passion for continuous learning, I aim to bridge the gap 
               between development and operations, enabling seamless, efficient, and scalable 
               software delivery.
